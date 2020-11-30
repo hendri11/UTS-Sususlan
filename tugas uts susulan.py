0c8ff7edadf2cd1e381bf3b1757afb698c7d9249
hendri11 <73053565+hendri11@users.noreply.github.com>
2020-11-30 21:02:50 +0700

    Add files via upload

---
+++ tugas uts susulan.py
@@ -0,0 +1,16 @@
+print('menentukan bilangan terbesar')
+max=0
+
+while True :
+
+    a=int(input('masukan bilangan= '))
+
+    if max < a:
+
+        max = a
+
+    if a==0:
+
+        break
+
+print('bilangan terbesar adalah= ',max)
